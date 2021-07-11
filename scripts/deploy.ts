a27287ceb6dc08537844cf33357c622a01f8447c
BitBD <info@kittytoken.io>
2021-07-12 04:32:08 +0600

    correct network name polygon > matic for graph API

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -12,7 +12,7 @@ async function start() {
     throw new Error('--chainId chain ID is required')
   }
   const path = `${process.cwd()}/.env${
-    args.chainId === 56 ? '.prod' : args.chainId === 97 ? '.dev' : '.local'
+    args.chainId === 1 ? '.prod' : args.chainId === 97 ? '.dev' : '.local'
   }`
 
   await require('dotenv').config({ path })
